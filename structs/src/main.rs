e6762d68382b187dc5f99023aea9f424ccc96146
alokraj-05 <alok200504@gmail.com>
2026-07-04 02:17:25 +0530

    Added more comments:)

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Student {
     name: String,
     age: u32,
@@ -16,7 +17,10 @@ fn main() {
     };
     // Created an instance of struct Student, now we can access the data of student1 using dot notaion
     let name = student1.name;
-    println!("My name is {}", name); // if we will not use the remaining values then it will show us never read warning. Try running code till here to witness that.
+    println!(
+        "My name is {} age: {}, grade: {}",
+        name, student1.age, student1.grade
+    ); // if we will not use the remaining values then it will show us never read warning. Try running code till here to witness that.
 
     // Create an mutable student record so we could change the data
     let mut student2 = Student {
@@ -29,10 +33,11 @@ fn main() {
     println!("Is {} alive ?: {}", student2.name, student2.alive);
 
     let student3 = add_student(String::from("Zeus"), 12); // creating student using a function that returns Student
-    let student4 = Student {
-        name: String::from("Thor"),
-        ..student1 // except name all other values will be copied from student1
-    };
+    // let student4 = Student {
+    //     name: String::from("Thor"),
+    //     ..student1 // except name all other values will be copied from student1
+    // };
+    println!("Data of student3: {:#?}", student3)
 }
 
 fn add_student(name: String, grade: u32) -> Student {
