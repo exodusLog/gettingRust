9b26c1c8290b9f89c84d4766de5671d4b473605f
alokraj-05 <alok200504@gmail.com>
2026-07-22 05:42:26 +0530

    Result error handling

--- error_handling/src/result.rs
+++ error_handling/src/result.rs
@@ -1,5 +1,6 @@
+use std::error;
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{Error, ErrorKind};
 
 pub fn result() {
     let greeting_file_result = File::open("hello.txt");
@@ -16,4 +17,14 @@ pub fn result() {
             }
         },
     };
+}
+pub fn another_res() {
+    let file = File::open("hello.txt").unwrap_or_else(|error: Error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt")
+                .unwrap_or_else(|error: Error| panic!("problem creating file {:?}", error))
+        } else {
+            panic!("Problem opening file: {:?}", error)
+        }
+    });
 }
